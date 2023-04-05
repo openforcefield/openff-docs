65c7e4082f8290c8cdb9d618d3a6c6bd91b91031
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-04-05 16:52:50 +1000

    Fix warning

--- source/conf.py
+++ source/conf.py
@@ -134,8 +134,3 @@ html_theme_options = {
 html_sidebars = {
     "**": ["globaltoc.html", "searchbox.html", "localtoc.html"],
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
