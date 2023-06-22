c4b938aecfb261cc6860ca64fb00440e389f6463
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-06-22 14:42:40 +1000

    Download micromamba in run_notebook.sh

--- source/_ext/micromamba/run_notebook.sh
+++ source/_ext/micromamba/run_notebook.sh
@@ -3,15 +3,23 @@
 set -Ee -o pipefail
 shopt -s failglob
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    MICROMAMBA=./micromamba-linux-64
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    MICROMAMBA=./micromamba-osx-64
+ARCH="64"
+OS=$(uname)
+
+if [[ "$OS" == "Linux" ]]; then
+    PLATFORM="linux"
+elif [[ "$OS" == "Darwin" ]]; then
+    PLATFORM="osx";
 else
-    echo "OS type $OSTYPE is not supported by $0"
+    echo "OS $OS is not supported by $0"
     echo "For detailed install instructions, see"
     echo "https://docs.openforcefield.org/en/latest/install.html"
     exit 1
+fi
+
+if [[ ! -f ./micromamba ]]; then
+    echo "Downloading micromamba"
+    curl -L# https://micro.mamba.pm/api/micromamba/$PLATFORM-$ARCH/latest | tar -xvj --strip-components=1 bin/micromamba
 fi
 
 PREFIX='./env'
@@ -19,7 +27,12 @@ PREFIX='./env'
 unset PYTHONPATH
 unset PYTHONHOME
 
-$MICROMAMBA create --file environment.yaml --prefix "$PREFIX"
-eval "$($MICROMAMBA shell hook --shell=bash)"
+if [[ -d $PREFIX/conda-meta/ ]]; then
+    ./micromamba update --file environment.yaml --prefix "$PREFIX"
+else
+    ./micromamba create --file environment.yaml --prefix "$PREFIX"
+fi
+
+eval "$(./micromamba shell hook --shell=bash)"
 micromamba activate "$PREFIX"
 jupyter notebook *.ipynb
