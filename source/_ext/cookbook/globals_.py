dbdf98335b5117e750b5b026ff260ebe51492109
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-03-29 08:20:54 -0500

    Try building against tag

--- source/_ext/cookbook/globals_.py
+++ source/_ext/cookbook/globals_.py
@@ -81,10 +81,7 @@ REPO_EXAMPLES_DIR = "examples"
 DEFAULT_CACHE_BRANCH = "_cookbook_data_main"
 """Branch of the openff-docs repository where cached notebooks are stored."""
 
-SKIP_NOTEBOOKS: set[str] = {
-    "openforcefield/openff-interchange/experimental/openmmforcefields/gaff.ipynb",
-    "openforcefield/openff-interchange/ligand_in_water/ligand_in_water.ipynb",
-}
+SKIP_NOTEBOOKS: set[str] = {}
 """
 Notebooks that will not be processed.
 
